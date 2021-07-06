11f22341ee12ba6b576749d4e18b93f965a91798
sabrinawhite4 <sabrinawhite4@me.com>
2021-07-06 09:50:28 -0600

    final

--- server/index.js
+++ server/index.js
@@ -18,30 +18,11 @@ const dogs = []
 
 app.get("/", (req,res) => {
     res.sendFile(path.join(__dirname, "../public/index.html"));
-})
-
-// app.get('/dogName', (req, res) => {
-//     const newDogName = generateDoggieName();
-//     res.status(200).send(newDogName);
-// });
+});
 
 app.post("/dogURL", (req, res) => {
     const {imageURL} = req.body;
     const dogURL = {imageURL};
     dogs.push(dogURL);
     res.status(200).send(dogs)
-    });
-
-// function capFirst(str) {
-//     return str.charAt(0).toUpperCase() + str.slice(1);
-// };
-
-// function getRandomInt(min, max) {
-//     return Math.floor(Math.random() * (max - min)) + min;
-// };
-
-// function generateDoggieName() {
-//     const name = ['franklin','simba','kobe','ozzy','rex','ollie','bubba','king','spot','rajah','mia', 'peanut','wally','louis', 'buster','bear', 'bertram','bark obama','chico','goose']
-//     const newName = capFirst(name[getRandomInt(0, name.length + 1)])
-//     return newName;
-// };
+});
